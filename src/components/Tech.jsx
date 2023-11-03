e2b66a26a167e5482e8fa4afa452af960b870015
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-03 21:02:22 +0100

    first commit

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,33 @@
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc"; // الحاوية 
+// import { technologies } from "../constants";
+
+// const Tech = () => {
+//   return (
+//     <div className='flex flex-row flex-wrap justify-center gap-10'>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "");
+
+
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc"; // الحاوية 
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const icons = Object.values(technologies).map((technology) => technology.icon);
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      <div className='w-full h-[75vh] mx-auto'>
+          <BallCanvas icons={icons} />
+      </div>
     </div>
   );
 };
