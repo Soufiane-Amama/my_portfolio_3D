eb0685ddb641e752238144779ff9179503f03e63
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-03 14:12:23 +0100

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       </div>
       <br />
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-2 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
